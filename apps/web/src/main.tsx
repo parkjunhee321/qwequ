226f79d843389f9995d54d90bc724d8ac1dc5ddb
parkjunhee321 <jhee.park@moyoplan.com>
2023-11-21 21:10:07 +0900

    dsa

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("app")!).render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter>
       <App />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
