f0150773ec3535e33b24f57c9d56c35b528f44b1
parkjunhee321 <jhee.park@moyoplan.com>
2023-11-21 20:35:34 +0900

    아몰랑

--- apps/test/src/App.tsx
+++ apps/test/src/App.tsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { ReactNode, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/login";
 import MainPage from "./pages/main";
 
 const CoreApp = React.lazy(() => import("@admin/core"));
 
+function DefaultFallback({ children }: { children: ReactNode }) {
+  return <Suspense fallback={<div>asd</div>}>{children}</Suspense>;
+}
+
+//v6에서는 exact -> *
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/*" element={<CoreApp />} />
-        <Route path="/test" element={<LoginPage />} />
-        <Route index element={<MainPage />} />
+        <Route path="/*">
+          <DefaultFallback>
+            <CoreApp />
+          </DefaultFallback>
+        </Route>
+        <Route path="/test">
+          <DefaultFallback>
+            <LoginPage />
+          </DefaultFallback>
+        </Route>
+        <Route path="/">
+          <DefaultFallback>
+            <MainPage />
+          </DefaultFallback>
+        </Route>
       </Routes>
     </>
   );
