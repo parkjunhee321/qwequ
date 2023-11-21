7d60fbc051fc77a38f6e3f4ed7e8d36b0809ac85
parkjunhee321 <jhee.park@moyoplan.com>
2023-11-21 18:41:01 +0900

    remove package

--- apps/test/src/App.tsx
+++ apps/test/src/App.tsx
@@ -1,8 +1,6 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/login";
-
-const CoreApp = React.lazy(() => import("@admin/core"));
+import CoreApp from "@admin/core";
 
 function App() {
   return (
